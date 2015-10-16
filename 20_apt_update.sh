ca42e9f2b99bf922bbc98a22f2995db7d581f022
Stian Larsen (lonix) <lonixx@gmail.com>
2015-10-16 14:48:38 +0200

    quite down apt-get a bit

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -2,5 +2,5 @@
 
 
 [ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
-[ "$APTLIST" ] && (apt-get update && apt-get --only-upgrade install -yqq $APTLIST)
+[ "$APTLIST" ] && (apt-get update -qq && apt-get --only-upgrade install -yqq $APTLIST)
 sleep 1s

bf75d7500214aa4caae80d2ac0f29702882c660a
Stian Larsen (lonix) <lonixx@gmail.com>
2015-10-19 10:42:26 +0200

    added echo for apt update

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -2,5 +2,5 @@
 
 
 [ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
-[ "$APTLIST" ] && (apt-get update -qq && apt-get --only-upgrade install -yqq $APTLIST)
+[ "$APTLIST" ] && (echo "We are now refreshing packages from apt repositorys, this *may* take a while" && apt-get update -qq && apt-get --only-upgrade install -yqq $APTLIST)
 sleep 1s

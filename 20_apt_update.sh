4a39d9877868e2507575318200769568776d98d2
Stian Buch Larsen <lonixx@gmail.com>
2015-12-14 09:47:54 +0100

    ADITIONAL_PACKAGES First include

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -2,5 +2,6 @@
 
 [ "$DISABLE_UPDATES" ] && exit 0
 [ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
+[ "$ADITIONAL_PACKAGES"] && APTLIST="$ADITIONAL_PACKAGES ""$APTLIST"
 [ "$APTLIST" ] && (echo "We are now refreshing packages from apt repositorys, this *may* take a while" && apt-get update -qq && apt-get --only-upgrade install -yqq $APTLIST)
 sleep 1s

30470c7c5da396dd657f8b1f36055d925607d3c7
sparklyballs <sparklyballs@gmail.com>
2015-12-20 16:36:59 +0000

    modified apt update script

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -4,33 +4,43 @@
 [ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
 [ "$APTLIST" ] || exit 0
 
+
 # set our functions
-reset_mirrors(){
-[[ -f /etc/apt/sources.list.d/backup_sources.list ]] && rm /etc/apt/sources.list.d/backup_sources.list
-sed -i '{/mirrors.ubuntu.com/ s/^#//}' /etc/apt/sources.list
-  }
+update_apt(){
+unset RETVAL
+apt-get update -qq
+RETVAL="$?"
+}
 
 delete_mirrors(){
-cp /defaults/backup_sources.list /etc/apt/sources.list.d/backup_sources.list 
-sed -i '/^#/! {/mirrors.ubuntu.com/ s/^/#/}' /etc/apt/sources.list  
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+mkdir -p /var/lib/apt/lists/partial
+sed -i '/^#/! {/mirrors.ubuntu.com/ s/^/#/}' /etc/apt/sources.list
   }
 
+reset_mirrors(){
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+mkdir -p /var/lib/apt/lists/partial
+sed -i '{/mirrors.ubuntu.com/ s/^#//}' /etc/apt/sources.list
+  }
 
 # reset to standard mirror setup
 reset_mirrors
 
-echo "We are now refreshing packages from apt repositorys, this *may* take a while"
+echo "We are now refreshing packages from apt repositories, this *may* take a while"
 
 # try apt-get update and output exit code to variable
-apt-get clean
-apt-get update -qq 
+update_apt
 
-
-# if update with mirrors failed, try backup list
-if [ "$?" != "0" ]; then
+# if update with mirrors failed, sed them out
+if [ "$RETVAL" != "0" ]; then
 delete_mirrors
-apt-get clean
-apt-get update -qq
+update_apt
 fi
 
 apt-get --only-upgrade install -yqq $APTLIST
+
+
+

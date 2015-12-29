315a6632da44b55c2b58579d92a85f5735e710ee
sparklyballs <sparklyballs@gmail.com>
2015-12-29 05:21:26 +0000

    fix advanced_script.sh, still needs modifying as echoing running script when not, but needed a last command

--- 999_advanced_script.sh
+++ 999_advanced_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-[ "$ADVANCED_SCRIPT" ] && echo "Scripting Enabled by: \$ADVANCED_SCRIPT"
-[ -f /config/userscript.sh ] && chmod +x /config/userscript.sh && echo "Userscript Provided"
-
+[ "$ADVANCED_SCRIPT" ] && echo "Scripting Enabled by: $ADVANCED_SCRIPT"
+[ -f /config/userscript.sh ] && (chmod +x /config/userscript.sh && echo "Userscript Provided")
 [ "$ADVANCED_SCRIPT" ] && [ -x /config/userscript.sh ] && /config/userscript.sh
+echo "Running userscript"
+
+

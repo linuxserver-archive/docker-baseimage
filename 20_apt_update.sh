fcb2f5077d0d03be28a8fb0508ba4a3e7709079b
sparklyballs <sparklyballs@gmail.com>
2015-12-29 05:19:19 +0000

    latest iteration of failover script

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -10,38 +10,43 @@
 
 # set our functions
 update_apt(){
-unset RETVAL
+unset RETVAL UPDATE_RETVAL UPGRADE_RETVAL
 apt-get update -qq
-RETVAL="$?"
+UPDATE_RETVAL=$?
+apt-get --only-upgrade install -yqq $APTLIST || true
+UPGRADE_RETVAL=$?
+RETVAL=$(($UPDATE_RETVAL+$UPGRADE_RETVAL))
 }
 
 delete_mirrors(){
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 mkdir -p /var/lib/apt/lists/partial
+cp /defaults/failover.list /etc/apt/sources.list.d/failover.list
 sed -i '/^#/! {/mirrors.ubuntu.com/ s/^/#/}' /etc/apt/sources.list
   }
 
 reset_mirrors(){
+  apt-get clean
+rm -rf /var/lib/apt/lists/*
+mkdir -p /var/lib/apt/lists/partial
+[[ -f /etc/apt/sources.list.d/failover.list ]] && rm /etc/apt/sources.list.d/failover.list
 sed -i '{/mirrors.ubuntu.com/ s/^#//}' /etc/apt/sources.list
   }
 
 # reset to standard mirror setup
 reset_mirrors
 
-echo "We are now refreshing packages from apt repositories, this *may* take a while"
+echo "We are now refreshing packages from apt repositorys, this *may* take a while"
 
-# try apt-get update and output exit code to variable
+# try initial update and upgrade.
 update_apt
 
 # if update with mirrors failed, sed them out
 if [ "$RETVAL" != "0" ]; then
 delete_mirrors
 update_apt
+else
+exit 0
 fi
-
-apt-get --only-upgrade install -yqq $APTLIST
-
-
-
 

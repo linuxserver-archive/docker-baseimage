cf7d8a9a9d90f730b75b2c84210825d139d30e29
Stian Buch Larsen <lonixx@gmail.com>
2015-12-29 21:17:28 +0100

    Fixed 999 to version from advancedaptbranch

--- 999_advanced_script.sh
+++ 999_advanced_script.sh
@@ -3,4 +3,4 @@
 [ "$ADVANCED_SCRIPT" ] && echo "Scripting Enabled by: $ADVANCED_SCRIPT"
 [ -f /config/userscript.sh ] && (chmod +x /config/userscript.sh && echo "Userscript Provided")
 [ "$ADVANCED_SCRIPT" ] && [ -x /config/userscript.sh ] && /config/userscript.sh
-echo "Running userscript"
+exit 0

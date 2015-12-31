37dca4eab310da8b29bb63c8abb86d6a1bc74b7f
sparklyballs <sparklyballs@gmail.com>
2015-12-31 05:19:53 +0000

    redesigned apt update script, attempting to use apt-select

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -7,10 +7,19 @@
 [ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
 [ "$APTLIST" ] || exit 0
 
+# install apt-select if required
+if [ ! -f "/app/apt-select/apt-select.py" ]; then
+curl -o /tmp/apt-select.zip -L https://github.com/jblakeman/apt-select/archive/master.zip
+cd /tmp
+unzip /tmp/apt-select.zip
+mv /tmp/apt-master /app/apt-select
+fi
 
 # set our functions
 update_apt(){
-# unset RETVAL UPDATE_RETVAL UPGRADE_RETVAL
+cd /app/apt-select python3 apt-select.py -t 3 -m up-to-date || true
+[[ -f /app/apt-select/sources.list ]] && mv /app/apt-select/sources.list /etc/apt/sources.list 
+unset RETVAL UPDATE_RETVAL UPGRADE_RETVAL
 apt-get update -qq
 UPDATE_RETVAL=$?
 apt-get --only-upgrade install -yqq $APTLIST
@@ -26,7 +35,7 @@ rm /etc/apt/sources.list.d/build.list
   }
 
 # reset to standard mirror setup
-# [[ -f /etc/apt/sources.list.d/build.list ]] && remove_build_list
+[[ -f /etc/apt/sources.list.d/build.list ]] && remove_build_list
 
 echo "We are now refreshing packages from apt repositorys, this *may* take a while"
 

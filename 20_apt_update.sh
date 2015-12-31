206837542eeb2e56b39b70a93d7ebd386b0b6d89
sparklyballs <sparklyballs@gmail.com>
2015-12-31 20:33:46 +0000

    replace the tests in update script

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+[ "$ADVANCED_DISABLEUPDATES" ] && exit 0
+[ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
+[ "$APTLIST" ] || exit 0
+
 cd /defaults
-echo "We are now refreshing packages from apt repositorys, this *may* take a while"
+echo "finding fastest mirror"
 python3 /app/aptselect/apt-select.py -t 3 -m up-to-date
 [[ -f /defaults/sources.list ]] && mv /defaults/sources.list /etc/apt/sources.list
+
+echo "We are now refreshing packages from apt repositorys, this *may* take a while"
 apt-get update -qq
 apt-get --only-upgrade install -yqq $APTLIST
 

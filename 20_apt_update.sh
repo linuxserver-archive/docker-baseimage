97f2108eb01caafc45a2a8864d974172fbfd3671
sparklyballs <sparklyballs@gmail.com>
2016-01-02 12:21:16 +0000

    commenting apt update script

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# test if we need to run the update scripts
 [ "$ADVANCED_DISABLEUPDATES" ] && exit 0
 [ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
 [ "$APTLIST" ] || exit 0
 
+# run apt-select script to find fastest up to date mirror
 cd /defaults
 echo "finding fastest mirror"
 python3 /app/aptselect/apt-select.py -t 3 -m up-to-date
 [[ -f /defaults/sources.list ]] && mv /defaults/sources.list /etc/apt/sources.list
 
+# check for and install any updates
 echo "We are now refreshing packages from apt repositorys, this *may* take a while"
 apt-get update -qq
 apt-get --only-upgrade install -yqq $APTLIST || echo "Something went wrong with the update, please try again later" && exit

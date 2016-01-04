513bb1fc4df971fed711e2501297ea834a4c17c7
sparklyballs <sparklyballs@gmail.com>
2016-01-04 09:33:10 +0000

    fix merge issues between branches

--- 20_apt_update.sh
+++ 20_apt_update.sh
@@ -4,7 +4,6 @@
 [ "$ADVANCED_DISABLEUPDATES" ] && exit 0
 [ "$BASE_APTLIST" ] && APTLIST="$BASE_APTLIST ""$APTLIST"
 [ "$APTLIST" ] || exit 0
-<<<<<<< HEAD
 
 # run apt-select script to find fastest up to date mirror
 cd /defaults
@@ -17,12 +16,5 @@ echo "We are now refreshing packages from apt repositories, this *may* take a wh
 apt-get update -qq
 apt-get --only-upgrade install -yqq $APTLIST || echo "Something went wrong with the update, please try again later" && exit
 
-
-
-=======
-echo "We are now refreshing packages from apt repositorys, this *may* take a while" 
-apt-get update -qq && apt-get --only-upgrade install -yqq $APTLIST
-if [ $? -eq 0 ]; then echo "Update: OK "; else echo "Update: ERROR "; fi
->>>>>>> master
 
 
